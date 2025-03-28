9f855a81408de7dec416f5c18a940a36d4da74ae
Lucas-Cunhaa <lucasgalvao260@gmail.com>
2025-03-28 14:55:15 -0300

    feat: added the user type

--- api/server.ts
+++ api/server.ts
@@ -1,14 +1,46 @@
 import { ApolloServer, gql } from "apollo-server";
 
+type User = {
+    id: number,
+    name: string,
+}
+const users: User[] = []
+
 const typeDefs = gql`
-    type Query{
-        helloWord: String!
+    type User {
+        id: Int!
+        name: String!
     }
+    type Query{
+       users: [User!]!
+    }
+    type Mutation {
+    createUser(id: String, name: String): User
+}
 `
 const server = new ApolloServer({
     typeDefs, 
+    resolvers: {
+        Query: {
+            users: () => {
+                return users
+            }
+        },
+
+        Mutation: {
+            createUser: (parent, args, ctx) => {
+                const user = {
+                    id: parseInt(args.id), 
+                    name: args.name
+                }
+                users.push(user)
+
+                return user
+            }
+        }
+    }
 })
 
-server.listen().then((URL) => {
-    console.log(`SERVER RUNING ON ${URL}`)
+server.listen().then(({url}) => {
+    console.log(`SERVER RUNING ON ${url}`)
 })
